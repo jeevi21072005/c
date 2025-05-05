6b7bed12713647eba7972cad8692dc3b72be3848
jeevi21072005 <jeevithamannar21@gmail.com>
2025-05-05 12:56:46 +0530

    Add files via upload

---
+++ secondprogram.c
@@ -0,0 +1,9 @@
+//welcome to my embedded program
+#include <stdio.h>
+int main (){
+    int a,b;
+    printf("Enter the two numbers:");
+    scanf("%d %d",&a,&b);
+    printf("%d",a-b);
+    return 0;
+}
